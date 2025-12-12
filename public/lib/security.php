0430b6a03415a92bc74ae7b8ef0ff9d5304c09b8
FrostFoe <frostfoe@gmail.com>
2025-12-12 08:47:40 +0000

    Refactor and harden security of sanitize_html function

--- public/lib/security.php
+++ public/lib/security.php
@@ -6,188 +6,97 @@ function sanitize_html($html)
         return "";
     }
 
-    // Suppress warnings for malformed HTML
     $dom = new DOMDocument();
     libxml_use_internal_errors(true);
 
-    // Hack to handle UTF-8 correctly
-    $dom->loadHTML(
-        mb_convert_encoding("<div>$html</div>", "HTML-ENTITIES", "UTF-8"),
-        LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD,
-    );
+    // A hack to handle UTF-8 characters correctly.
+    // The alternative is to use the 'encoding' parameter in loadHTML,
+    // but that requires a proper HTML document with a meta charset tag.
+    if (!$dom->loadHTML(mb_convert_encoding("<div>$html</div>", 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD)) {
+        // If loading fails, return an empty string for safety.
+        // You might want to log the errors from libxml_get_errors() here.
+        libxml_clear_errors();
+        return "";
+    }
+
     libxml_clear_errors();
 
     $allowed_tags = [
-        "p",
-        "b",
-        "strong",
-        "i",
-        "em",
-        "u",
-        "a",
-        "ul",
-        "ol",
-        "li",
-        "h1",
-        "h2",
-        "h3",
-        "h4",
-        "h5",
-        "h6",
-        "br",
-        "img",
-        "blockquote",
-        "span",
-        "div",
+        "p", "b", "strong", "i", "em", "u", "a", "ul", "ol", "li",
+        "h1", "h2", "h3", "h4", "h5", "h6", "br", "img",
+        "blockquote", "span", "div"
     ];
-    $allowed_attrs = ["href", "src", "alt", "title", "class", "target"];
+
+    $allowed_attrs = ["href", "src", "alt", "title", "class", "target", "rel"];
+
+    // A list of tags to be removed completely with their content.
+    $strip_tags = ['script', 'style', 'iframe', 'object', 'embed', 'applet', 'link', 'meta'];
 
     $xpath = new DOMXPath($dom);
-    $nodes = $xpath->query("//*");
+    $nodes = $xpath->query('//*');
 
-    foreach ($nodes as $node) {
-        if (!in_array($node->nodeName, $allowed_tags)) {
-            // Remove the tag but keep content (except for script/style which should be gone entirely)
-            if (
-                in_array($node->nodeName, [
-                    "script",
-                    "style",
-                    "iframe",
-                    "object",
-                    "embed",
-                    "applet",
-                    "meta",
-                    "link",
-                ])
-            ) {
-                $node->parentNode->removeChild($node);
-            } else {
-                // For other tags, we might want to unwrap, but for simplicity/safety, let's just strip invalid tags but keep text
-                // Actually, DOMDocument doesn't easily "unwrap".
-                // Let's just remove dangerous nodes entirely to be safe, or replace with text.
-                // Replacing with text is better UX but harder.
-                // Let's iterate backwards to safely remove.
-            }
-        } else {
-            // Allowed tag, check attributes
-            if ($node->hasAttributes()) {
-                $attrs = [];
-                foreach ($node->attributes as $attr) {
-                    $attrs[] = $attr->name;
-                }
-                foreach ($attrs as $attr) {
-                    if (!in_array($attr, $allowed_attrs)) {
-                        $node->removeAttribute($attr);
-                    } elseif ($attr === "href" || $attr === "src") {
-                        $value = $node->getAttribute($attr);
-                        // Basic protocol check
-                        if (
-                            preg_match(
-                                "/^(javascript|vbscript|data):/i",
-                                $value,
-                            )
-                        ) {
-                            $node->removeAttribute($attr);
-                        }
-                    }
-                }
-            }
-        }
-    }
+    for ($i = $nodes->length - 1; $i >= 0; $i--) {
+        $node = $nodes->item($i);
 
-    // Second pass to remove forbidden tags that might have been skipped or structural issues
-    // Actually, the loop above modifies the DOM. Iterating while modifying can be tricky.
-    // A better approach:
-
-    $process_node = function ($node) use (
-        &$process_node,
-        $allowed_tags,
-        $allowed_attrs,
-    ) {
-        if ($node->nodeType === XML_ELEMENT_NODE) {
-            if (!in_array($node->nodeName, $allowed_tags)) {
-                return false; // Mark for removal
-            }
-
-            // Clean attributes
-            $attrs = [];
-            if ($node->hasAttributes()) {
-                foreach ($node->attributes as $attr) {
-                    $attrs[] = $attr->name;
-                }
-                foreach ($attrs as $name) {
-                    if (!in_array($name, $allowed_attrs)) {
-                        $node->removeAttribute($name);
-                    } else {
-                        $val = $node->getAttribute($name);
-                        if (
-                            preg_match("/^(javascript|vbscript|data):/i", $val)
-                        ) {
-                            $node->removeAttribute($name);
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    };
-
-    // Re-do with a safer loop: get all elements, iterate backwards
-    $all = $xpath->query("//*");
-    for ($i = $all->length - 1; $i >= 0; $i--) {
-        $node = $all->item($i);
-        if (
-            in_array($node->nodeName, [
-                "script",
-                "style",
-                "iframe",
-                "object",
-                "embed",
-                "applet",
-                "link",
-            ])
-        ) {
+        // Remove dangerous tags and their content
+        if (in_array(strtolower($node->nodeName), $strip_tags)) {
             $node->parentNode->removeChild($node);
-        } elseif (!in_array($node->nodeName, $allowed_tags)) {
-            // Replace with its children (unwrap)
-            $fragment = $dom->createDocumentFragment();
-            while ($node->firstChild) {
-                $fragment->appendChild($node->firstChild);
-            }
-            $node->parentNode->replaceChild($fragment, $node);
-        } else {
-            // Clean attributes
-            if ($node->hasAttributes()) {
-                // Copy to array to avoid iterator issues
-                $attrs = [];
-                foreach ($node->attributes as $attr) {
-                    $attrs[] = $attr->name;
+            continue;
+        }
+
+        // Unwrap unwanted tags but keep their content
+        if (!in_array(strtolower($node->nodeName), $allowed_tags)) {
+            if ($node->hasChildNodes()) {
+                $fragment = $dom->createDocumentFragment();
+                while ($node->firstChild) {
+                    $fragment->appendChild($node->firstChild);
                 }
-                foreach ($attrs as $name) {
-                    if (!in_array($name, $allowed_attrs)) {
-                        $node->removeAttribute($name);
-                    } else {
-                        $val = $node->getAttribute($name);
-                        if (
-                            preg_match("/^(javascript|vbscript|data):/i", $val)
-                        ) {
-                            $node->removeAttribute($name);
-                        }
+                $node->parentNode->replaceChild($fragment, $node);
+            } else {
+                $node->parentNode->removeChild($node);
+            }
+            continue;
+        }
+
+        // Sanitize attributes of allowed tags
+        if ($node->hasAttributes()) {
+            foreach (iterator_to_array($node->attributes) as $attr) {
+                if (!in_array(strtolower($attr->name), $allowed_attrs)) {
+                    $node->removeAttribute($attr->name);
+                    continue;
+                }
+
+                // Sanitize 'href' and 'src' attributes
+                if (in_array(strtolower($attr->name), ['href', 'src'])) {
+                    $url = $attr->value;
+                    // A more robust regex to filter out dangerous protocols.
+                    // It also checks for html entities.
+                    if (preg_match('/^\s*(?:javascript|vbscript|data|file|php|phar|zlib|glob|ssh2|expect|ogg|ftp|sftp):/i', $url)) {
+                        $node->removeAttribute($attr->name);
+                    }
+                    // Optional: You might also want to ensure that the URL is well-formed.
+                    // and points to http, https, or mailto.
+                    elseif (!preg_match('/^\s*(?:https?|mailto|#|\/)/i', $url)) {
+                        $node->removeAttribute($attr->name);
                     }
                 }
+
+                // Sanitize 'target' attribute to prevent tabnabbing
+                if (strtolower($attr->name) === 'target' && strtolower($attr->value) === '_blank') {
+                    $node->setAttribute('rel', 'noopener noreferrer');
+                }
             }
         }
     }
 
-    // Get body content only
-    $body = $dom->getElementsByTagName("div")->item(0);
+    // Extract the sanitized HTML content from the div wrapper.
+    $body = $dom->getElementsByTagName('div')->item(0);
+    $sanitized_html = '';
     if ($body) {
-        $output = "";
         foreach ($body->childNodes as $child) {
-            $output .= $dom->saveHTML($child);
+            $sanitized_html .= $dom->saveHTML($child);
         }
-        return $output;
     }
 
-    return "";
+    return $sanitized_html;
 }
